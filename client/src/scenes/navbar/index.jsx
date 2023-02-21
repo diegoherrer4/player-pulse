4fcd7f7214caa8b0ef8dbf193c541556da2c5156
Diego Herrera <dagomessis@gmail.com>
2023-02-21 12:34:32 -0500

    Changes

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -39,8 +39,7 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  // const fullName = `${user.firstName} ${user.lastName}`;
-   const fullName = "Diego herrera"
+  const fullName = `${user.firstName} ${user.lastName}`;
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
@@ -48,7 +47,7 @@ const Navbar = () => {
         <Typography
           fontWeight="bold"
           fontSize="clamp(1rem, 2rem, 2.25rem)"
-          color="green"
+          color="primary"
           onClick={() => navigate("/home")}
           sx={{
             "&:hover": {
@@ -57,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Player Pulse
+          Sociopedia
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
