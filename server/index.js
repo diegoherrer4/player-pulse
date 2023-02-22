d9c01ee557043f7eb5363ff0ef05c4dd8c14f78a
Diego Herrera <dagomessis@gmail.com>
2023-02-21 19:11:41 -0500

    changes

--- server/index.js
+++ server/index.js
@@ -31,13 +31,16 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use(
+  "/assets",
+  express.static(path.join(__dirname, "public-server/assets"))
+);
 
 /*  FILE STORAGE */
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/assets");
+    cb(null, "public-server/assets");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
