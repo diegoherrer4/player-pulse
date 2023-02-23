6defa643ee38f81a82ddf5774f101f6d5e4e9c82
Diego Herrera <dagomessis@gmail.com>
2023-02-23 00:33:59 -0500

    not working

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,8 @@
 import express from "express";
 import { login } from "../controllers/auth.js";
-import { register } from "../controllers/auth.js";
-import multer from "multer";
+
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public-server/assets");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-
-// const upload = multer({ storage });
-
-/* ROUTES WITH FILES */
-router.post("/register", register);
 router.post("/login", login);
 
 export default router;
