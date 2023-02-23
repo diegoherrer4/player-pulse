3bb5cfbf357b61aa2e9a6ca9e7df286c60fe7a46
Diego Herrera <dagomessis@gmail.com>
2023-02-22 20:47:26 -0500

    changed again

--- index.js
+++ index.js
@@ -29,9 +29,12 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", function (_, res) {
-  res.sendFile(path.join(__dirname, "./build/index.html"), function (err) {
-    res.status(500).send(err);
-  });
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
 });
 
 app.use(helmet());
