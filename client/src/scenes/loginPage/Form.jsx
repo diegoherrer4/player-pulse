1e973d9711ceeaf089841319b31fc4206bd1aed7
Diego Herrera <dagomessis@gmail.com>
2023-02-22 23:46:31 -0500

    s

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -63,10 +63,13 @@ const Form = () => {
     }
     formData.append("picturePath", values.picture.name);
 
-    const savedUserResponse = await fetch("auth/register", {
-      method: "POST",
-      body: formData,
-    });
+    const savedUserResponse = await fetch(
+      "https://https://jade-dark-firefly.cyclic.app/auth/register",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
     const savedUser = await savedUserResponse.json();
     onSubmitProps.resetForm();
 
