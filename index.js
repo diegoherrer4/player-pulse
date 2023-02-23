4098fcdaf7d77ac3b0dc6804014d8831ee539408
Diego Herrera <dagomessis@gmail.com>
2023-02-22 21:16:02 -0500

    s

--- index.js
+++ index.js
@@ -28,12 +28,11 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "client/build")));
 app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
+  res.sendFile(path.join(__dirname, "client/build/index.html"), function (err) {
+    if (err) {
       res.status(500).send(err);
     }
-  );
+  });
 });
 
 app.use(helmet());
